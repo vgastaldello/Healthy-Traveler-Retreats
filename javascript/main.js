c005183564968437ccd09854a6eaa2234de7dab9
Val Anne <val.gastaldello@gmail.com>
2017-07-12 23:20:35 +0800

    tooo loopy
    lots of work

---
+++ javascript/main.js
@@ -0,0 +1,14 @@
+$("document").ready(function(){
+
+
+
+$("#typed").typed( {
+   strings: ["Adventure.", "Education.", "Inspiration.", "Transformation."],
+   typeSpeed: 50,
+   backDelay: 1000,
+   loop: true,
+   // defaults to false for infinite loop
+   loopCount: false,
+   });
+
+ });
